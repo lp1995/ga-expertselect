e5b129f39c0a3eac53da1fee1d3c98c5ab15f3dd
liupeng <liupengfun@163.com>
2018-11-05 21:37:27 +0800

    11

--- src/main/java/liupeng/ga/cn/expertselect/Run.java
+++ src/main/java/liupeng/ga/cn/expertselect/Run.java
@@ -8,7 +8,7 @@ public class Run
 {
     public static void main( String[] args )
     {
-    	Population pop = new Population();
+    	Population pop = new Population(100,false);
     	GAlgorithm ga = new GAlgorithm();
     	
         System.out.println( "Hello World!" );
